fbcfe0c8cc403d06043f1471a1fc6f152c16c243
Safin Bhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2022-11-07 00:15:54 +0600

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	#ifndef KHELA_CHOLBE
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout);
+	#endif
+
+        cout<<"Hello World!";
+
+        return 0;
+ }
