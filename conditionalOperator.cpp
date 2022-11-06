48fd56bb44cfca94cadab2122639a5a6ac68b687
Safin Bhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2022-11-07 00:52:46 +0600

    Create conditionalOperator.cpp

---
+++ conditionalOperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout);
+	#endif
+
+        int a;
+        cin>>a;
+        if(a%2==0 && a>2){
+            cout<<"YES";
+        }
+        else{
+            cout<<"NO";
+        }
+
+        return 0;
+ }
